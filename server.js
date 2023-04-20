3717f3c7dfb81c57a9d7ffc884b8b42c588d61f0
yskkkk <102001243+yskkkk@users.noreply.github.com>
2023-04-21 00:18:52 +0900

    Update server.js
    사진 업로드 부분 경로 diary에 넣게끔 수정

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/moms/ccyyhh', async (req, res) => {
     const jsonData = [];
     const columnNames = result.metaData.map((col) => col.name);
     for (let row of result.rows) {
-      const obj = {};
+    const obj = {};
       for (let i = 0; i < columnNames.length; i++) {
         obj[columnNames[i]] = row[i];
       }
@@ -81,6 +81,7 @@ app.get('/moms/ccyyhh', async (req, res) => {
 });
 
 app.get('/moms/register-info', async (req, res) => {
+  let log =``;
   try {
     const now = new Date();
     const month = (now.getMonth() + 1).toString().padStart(2, '0');
@@ -101,7 +102,7 @@ app.get('/moms/register-info', async (req, res) => {
     const jsonData = [];
     const columnNames = result.metaData.map((col) => col.name);
     for (let row of result.rows) {
-      const obj = {};
+    const obj = {};
       for (let i = 0; i < columnNames.length; i++) {
         obj[columnNames[i]] = row[i];
       }
@@ -109,7 +110,7 @@ app.get('/moms/register-info', async (req, res) => {
     }
     res.send(jsonData);
     
-  let log = `/moms/register-info ->[ ${ip} ] register 조회 < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`;
+  log += `/moms/register-info ->[ ${ip} ] register 조회 < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`;
 }
    catch (err) {
     console.error(err.message);
@@ -144,7 +145,7 @@ app.get('/moms/yysskk', async (req, res) => {
     const jsonData = [];
     const columnNames = result.metaData.map((col) => col.name);
     for (let row of result.rows) {
-      const obj = {};
+    const obj = {};
       for (let i = 0; i < columnNames.length; i++) {
         obj[columnNames[i]] = row[i];
       }
@@ -179,6 +180,7 @@ app.post('/moms/ccyyhh/register', async (req, res) => {
     };
     const result = await connection.execute(sql, bindParams, { autoCommit: true });
 
+  
     let log = `/moms/ccyyhh/register ->[ ${ip} ] age-cmt 삽입 ${JSON.stringify(req.body)} < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`;
     fs.appendFile(logFilePath, log, (err) => {
     if (err) throw err;
@@ -217,7 +219,7 @@ app.post('/moms/register', async (req, res) => {
       email: email
     };
     const result = await connection.execute(sql, bindParams, { autoCommit: true });
-    res.status(200).send(`Inserted ${result.rowsAffected} row(s)`);
+    res.status(200).send(`Inserted ${result.rowsAffecte} row(s)`);
     
     log +=` [성공] < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`;
      
@@ -245,18 +247,13 @@ app.post('/moms/login', async (req, res) => {
   const ip = req.connection.remoteAddress;
   const connection = await OracleDB.getConnection(dbConfig);
   let log = `/moms/login ->[ ${ip} ] 로그인 요청 ${JSON.stringify(req.body)}`;
-  fs.appendFile(logFilePath, log, (err) => {
-    if (err) throw err;
-    console.log(log); // 로그를 콘솔에 출력
-  });
   try {
-
     const result = await connection.execute(
       `SELECT id, pw FROM register WHERE id = :id`,
       [id]
     );
 
-    if (result.rows.length > 0) {
+    if (result.rows.length> 0) {
       const login = await connection.execute(
         `SELECT * FROM register WHERE id = :id and pw = :pw`,
         [id, crypto.createHash('md5').update(pw).digest('hex')]
@@ -322,7 +319,7 @@ app.post('/moms/find-id', async (req, res) => {
       [email, name, phone]
     );
 
-    if (result.rows.length > 0) {
+    if (result.rows.length> 0) {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         const logininfo = {
           success: true
@@ -393,11 +390,10 @@ app.post('/moms/find-id/id', async (req, res) => {
     );
     const info = {
       success: true,
-      id: result.rows[0][0],
-    };
-    if(result.rows.length > 0)
+      id: result.rows[0][0],    };
+    if(result.rows.length> 0)
     {
-      log = `/moms/find-id/id ->[ ${ip} ] 아이디 반환 -> [성공] ${result.rows[0][0]} < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
+      log = `/moms/find-id/id ->[ ${ip} ] 아이디 반환 -> [성공] ${result.rows[0][0]}< ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
     }
     else{
       log = `/moms/find-id/id ->[ ${ip} ] 아이디 반환 -> [실패] < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
@@ -510,7 +506,7 @@ app.post('/moms/change-pw/pw', async (req, res) => {
   };
   const result = await connection.execute(sql, bindParams, { autoCommit: true });
      
-  if (result.rowsAffected >0){
+  if (result.rowsAffecte >0){
     res.writeHead(200, { 'Content-Type': 'application/json' });
     const info = {
        success: true,
@@ -541,6 +537,7 @@ app.post('/moms/change-pw/pw', async (req, res) => {
 //사진 업로드
 app.post('/upload/images', upload.array('images', 10), async (req, res) => {
   let log = ``;
+  let fileNames=``;
   try {
     const ip = req.connection.remoteAddress;
     const now = new Date();
@@ -550,10 +547,18 @@ app.post('/upload/images', upload.array('images', 10), async (req, res) => {
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
     const files = req.files;
-    const fileNames = req.files.map(file => file.originalname);
+    const clientNum = parseInt(req.body.clientNum);
+    const diary_date = req.body.diary_date;
+    const connection = await OracleDB.getConnection();
 
+    const sql = `SELECT diaryNo FROM diary WHERE clientNum = :clientNum and diary_date = :diary_date`;
+      const binds = {
+        clientNum: clientNum,
+        diary_date: diary_date
+      };
+    const options = { autoCommit: true };
+    const Sresult = await connection.execute(sql, binds, options);
     log += `/upload/images -> [ ${ip} ] -> `;
-
     if (!files) {
       log += `[실패] 이미지 없음 < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
       res.status(400).send('No file uploaded.');
@@ -562,29 +567,41 @@ app.post('/upload/images', upload.array('images', 10), async (req, res) => {
         console.log(log); // 로그를 콘솔에 출력
       });
       return;
-    }
-
-    log += `[성공] 이미지 업로드 ${fileNames} < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
-    fs.appendFile(logFilePath, log, (err) => {
-      if (err) throw err;
-      console.log(log); // 로그를 콘솔에 출력
-    });
-    const filePaths = files.map(file => file.path);
-    
-    const sql = `INSERT INTO image_path (path) VALUES (:path)`;
-    const binds = filePaths.map(filePath => ({ path: filePath }));
-    const options = { autoCommit: true };
-    const connection = await OracleDB.getConnection();
-    const result = await connection.executeMany(sql, binds, options);
-
-    if(result.rowsAffected>0)
-    {
-      const log = `/upload -> [ ${ip} ] -> [성공] 경로 저장 성공 : ${fileNames} < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
-    }
-    connection.close();
-    res.status(200).send(`${files.length} files uploaded!`);
+    }else{
+      const filePaths = files.map(file => file.path);
+      for (let i = 0; i < files.length; i++) {
+        const filename = filePaths[i].split('\\').pop().split('/').pop();
+        fileNames += `/${filename}`;
+        console.log(`${filename} ${i}번째 업로드`);
+      }
+      console.log(Sresult.rows.length)
+      if(Sresult.rows.length > 0){ // 입력받은 다이어리가 존재하면
+        log += `[성공] 이미지 업로드 ${fileNames} < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
+        fs.appendFile(logFilePath, log, (err) => {
+          if (err) throw err;
+          console.log(log); // 로그를 콘솔에 출력
+        });
+        log=``;
+      
+        const sql2 = `UPDATE diary SET imageURL = :fileNames WHERE diaryNo = :diaryNo`;
+        const binds2 = {
+          fileNames: fileNames,
+          diaryNo: Sresult.rows[0][0]
+        };
+        const result = await connection.execute(sql2, binds2, options);
   
-  } catch (err) {
+        if(result.rowsAffected > 0)
+        {
+          log = `/upload -> [ ${ip} ] -> [성공] 경로 저장 성공 : ${fileNames} < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
+        }
+        connection.close();
+        res.status(200).send(`${files.length} files uploaded!`);
+      }
+    else{
+      log += `[실패] 아직은 알 수 없는 오류 ${fileNames} < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`
+    }
+  }
+} catch (err) {
     console.error(err);
     res.status(500).send('Server Error');
   }
@@ -615,7 +632,6 @@ app.get('/image/:path', (req, res) => {
       res.writeHead(200, {'Content-Type': 'image/jpeg'});
       res.end(data);
     }
-
     fs.appendFile(logFilePath, log, (err) => {
       if (err) throw err;
       console.log(log); // 로그를 콘솔에 출력
@@ -751,7 +767,46 @@ app.post('/moms/sendemail/auth', (req, res) => {
     return res.end(JSON.stringify(info));
   }  
 });
+//다이어리 등록
+app.post('/moms/diary', async (req, res) => {
+  let log =`` ;
+  try {
+    const now = new Date();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+  
+    const { clientNum, diary_date, content} = req.body;
 
+    const ip = req.connection.remoteAddress; //client ip
+    // Oracle 데이터베이스 연결
+    const connection = await OracleDB.getConnection(dbConfig);
+    
+    log +=`/moms/diary ->[ ${ip} ] 다이어리 작성 ${JSON.stringify(req.body)} ->`
+    const sql = `INSERT INTO diary(clientNum, diary_date, content) VALUES (:clientNum, :diary_date, :content)`;
+    const bindParams = {
+      clientNum: clientNum,
+      diary_date: diary_date,
+      content: content
+    };
+    const result = await connection.execute(sql, bindParams, { autoCommit: true });
+    res.status(200).send(`Inserted ${result.rowsAffecte} row(s)`);
+    
+    log +=` [성공] < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`;
+     
+  } 
+   catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+    log +=` [실패] < ${now.getFullYear()}-${month}-${day} ${hours}:${minutes}:${seconds} >\n`;
+  }
+  fs.appendFile(logFilePath, log, (err) => {
+    if (err) throw err;
+    console.log(log); // 로그를 콘솔에 출력
+  });
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
